7da45ece75f2880becf0d96be8d8af326eca8be9
Camilo Andres Pardo <camilopardochoachi@gmail.com>
2020-06-21 21:42:02 -0500

    Inclución de estilos al inicio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,16 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+      <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback')}}">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
+
+  <!-- link local de JavaScrip y Css -->
+  <link rel="stylesheet" href="{{asset('Css/Estilos.css')}}">
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
